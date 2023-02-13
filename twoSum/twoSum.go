ee7201ce822ef3fc5fb2f9fc20552b0031e9f09f
yuzuki.cat <yuzuki.cat@kamisu66.com>
2023-02-13 22:58:08 +0900

    two sum

--- twoSum/twoSum.go
+++ twoSum/twoSum.go
@@ -6,19 +6,24 @@ import (
 
 func towSum(nums []int, target int) {
 	numMap := make(map[int]int)
+	solveSlice := []int{}
 	for index, number := range nums {
 		anotherNumber := target - number
 		numMap[number] = index
 		anotherIndex, exist := numMap[anotherNumber]
 		if exist && anotherIndex != index {
-			fmt.Println(index, anotherIndex)
+			solveSlice = append(solveSlice, anotherIndex)
+			solveSlice = append(solveSlice, index)
 			break
+		} else if exist && anotherIndex == index {
+			solveSlice = append(solveSlice, index)
 		}
 	}
+	fmt.Println(solveSlice)
 }
 
 func main() {
-	nums := []int{2, 7, 11, 5}
+	nums := []int{2, 7, 11, 15}
 	target := 9
 	towSum(nums, target)
 }
