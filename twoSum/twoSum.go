91ba949bf21ad0098a04c20c56e7a008d051ee48
yuzuki.cat <yuzuki.cat@kamisu66.com>
2023-02-13 22:11:46 +0900

    two sum

--- twoSum/twoSum.go
+++ twoSum/twoSum.go
@@ -1,1 +1,26 @@
-package twoSum
+package main
+
+import (
+	"fmt"
+)
+
+func towSum(nums []int, target int) {
+	numMap := make(map[int]int)
+	for index, number := range nums {
+		numMap[number] = index
+	}
+	for number, index := range numMap {
+		anotherNumber := target - number
+		anotherIndex, exist := numMap[anotherNumber]
+		if exist && anotherIndex != index {
+			fmt.Println(index, anotherIndex)
+			break
+		}
+	}
+}
+
+func main() {
+	nums := []int{2, 7, 11, 5}
+	target := 9
+	towSum(nums, target)
+}
