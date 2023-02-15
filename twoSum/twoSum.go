62059792329ecfb095affa6234daa1b292024ec8
yuzuki.cat <yuzuki.cat@kamisu66.com>
2023-02-15 20:08:22 +0900

    optimize01

--- twoSum/twoSum.go
+++ twoSum/twoSum.go
@@ -3,10 +3,10 @@ package twoSum
 func TowSum(nums []int, target int) []int {
 	hashMap := make(map[int]int)
 	for index, number := range nums {
-		_, exist := hashMap[number]
-		if exist {
-			return []int{hashMap[number], index}
+		if anotherIndex, exist := hashMap[number]; exist {
+			return []int{anotherIndex, index}
 		}
+		//Store opposite in hashMapcache
 		hashMap[target-number] = index
 	}
 	return nil
