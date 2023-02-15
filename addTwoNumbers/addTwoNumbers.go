8514cfba52f1874068e8e2877486c2c44bd9304d
yuzuki.cat <yuzuki.cat@kamisu66.com>
2023-02-15 13:31:52 +0900

    leet code 02 && readme

--- addTwoNumbers/addTwoNumbers.go
+++ addTwoNumbers/addTwoNumbers.go
@@ -1,19 +1,29 @@
-package main
+package addTwoNumbers
 
-import (
-	"github.com/yuzukicat/golang-leet-code-solve/addTwoNumbers/linkedList"
-)
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
 
-func main() {
-	linkedListOne := linkedList.List{}
-	linkedListOne.Insert(5)
-	linkedListOne.Insert(2)
-	linkedListOne.Insert(1)
-	linkedListTwo := linkedList.List{}
-	linkedListTwo.Insert(2)
-	linkedListTwo.Insert(7)
-	linkedListTwo.Insert(3)
-	linkedListOne.Display()
-	linkedListTwo.Display()
-	linkedList.AddTwoLinkedList(&linkedListOne, &linkedListTwo)
+func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	head := &ListNode{0, nil}
+	pointer := head
+	carry := 0
+	for l1 != nil || l2 != nil || carry > 0 {
+		sum := carry
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
+		carry = sum / 10
+		pointer.Next = new(ListNode)
+		pointer.Next.Val = sum % 10
+		pointer = pointer.Next
+	}
+	return head.Next
 }
--- twoSum/twoSum.md
+++ twoSum/twoSum.md
@@ -1,5 +1,16 @@
+# Two Sum
+
 Given an array of integers ``nums`` and an integer ``target``, return indices of the two numbers such that they add up to ``target``.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 You can return the answer in any order.
+
+# Constraints
+
+- 2 <= nums.length <= 104
+- -109 <= nums[i] <= 109
+- -109 <= target <= 109
+- Only one valid answer exists.
+
+# Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
